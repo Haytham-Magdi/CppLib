4286716ac40538617ef134b5b9078f8d7e9c6d16
haytham magdi <haytham.magdi@hotmail.com>
2016-08-11 23:28:03 +0300

    added: void CMfcCvTut_11Jun07Dlg::OnBnClickedTry26Btn_0() for backup

--- Hcv/MfcCvTut_11Jun07Dlg.cpp
+++ Hcv/MfcCvTut_11Jun07Dlg.cpp
@@ -1482,6 +1482,35 @@ void CMfcCvTut_11Jun07Dlg::OnBnClickedTry25Btn()
 
 }
 
+void CMfcCvTut_11Jun07Dlg::OnBnClickedTry26Btn_0()
+{
+	// TODO: Add your control notification handler code here
+
+	if (proc != 0)
+	{
+		// process and display
+
+
+		{
+			CString sTmp;
+
+			this->edtAprSize1D.GetWindowTextA(sTmp);
+
+			CString sTmp2 = sTmp.Trim();
+
+			//GlobalStuff::AprSize1D = atoi( sTmp2.GetBuffer() );
+			GlobalStuff::AprSize1D = atof(sTmp2.GetBuffer());
+		}
+
+
+		proc->Try26();
+
+		//proc->display();
+
+	}
+
+}
+
 void CMfcCvTut_11Jun07Dlg::OnBnClickedTry26Btn()
 {
 	// TODO: Add your control notification handler code here
