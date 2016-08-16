951c2e10654aad32113c2c209b62b0be8b874ac5
haytham magdi <haytham.magdi@hotmail.com>
2016-08-16 12:21:52 +0300

    modifying (( template<class T_ImgElm, class T_AccElm, int V_SupposedNofChannels> ))

--- Hcv/Image_MemAccessor_2D.h
+++ Hcv/Image_MemAccessor_2D.h
@@ -11,46 +11,71 @@
 #include <Lib\Hcv\Image.h>
 //#include <queue>      
 
+//#include <Lib\cpp\MemAccessor_2D.h>
+
+
 
 namespace Hcv
 {
 	using namespace Hcpl;
 	//using namespace std;
 
-	template<class T_ImgElm, class T_AccElm>
-	static void PrepareAccessorFromImage(
-		//Hcpl::ObjRef< Hcv::Image< T_ImgElm >> a_srcImg,
-		Hcv::Image< T_ImgElm > * a_srcImg,
-		int a_supposedNofChannels, MemAccessor_2D<T_AccElm> * a_pAccessor)
-	{
-		Hcpl_ASSERT(a_srcImg->GetNofChannels() == a_supposedNofChannels);
+	//template<class T_ImgElm, class T_AccElm>
 
-		a_pAccessor->Init((T_AccElm *)a_srcImg->GetPixAt(0, 0),
-			1, a_srcImg->GetSize().width, a_srcImg->GetSize().height);
-	}
-
-
-
+	template<class T_ImgElm, class T_AccElm, int V_SupposedNofChannels>
 	class Image_MemAccessor_2D : FRM_Object
 	{
 	public:
 
-		 
-		static void PrepareF32ColorValAccessor(F32ImageRef a_srcImg, MemAccessor_2D<F32ColorVal> * a_pAccessor)
+		Image_MemAccessor_2D(Hcpl::ObjRef< Hcv::Image< T_ImgElm >> a_srcImg)
 		{
-			Hcpl_ASSERT(3 == a_srcImg->GetNofChannels());
+			m_srcImg = a_srcImg;
+			Image_MemAccessor_2D::PrepareAccessorFromImage(m_srcImg, V_SupposedNofChannels, &m_memAccessor);
+		}
 
-			a_pAccessor->Init((F32ColorVal *)a_srcImg->GetPixAt(0, 0),
+		MemAccessor_2D<T_AccElm> &  GetMemAccessor()
+		{
+			return m_memAccessor;
+		}
+
+		static void PrepareAccessorFromImage(
+			Hcv::Image< T_ImgElm > * a_srcImg,
+			MemAccessor_2D<T_AccElm> * a_pAccessor)
+		{
+			Hcpl_ASSERT(a_srcImg->GetNofChannels() == V_SupposedNofChannels);
+
+			a_pAccessor->Init((T_AccElm *)a_srcImg->GetPixAt(0, 0),
 				1, a_srcImg->GetSize().width, a_srcImg->GetSize().height);
 		}
 
-		static void PrepareFloatAccessor(F32ImageRef a_srcImg, MemAccessor_2D<float> * a_pAccessor)
-		{
-			Hcpl_ASSERT(1 == a_srcImg->GetNofChannels());
 
-			a_pAccessor->Init((float *)a_srcImg->GetPixAt(0, 0),
-				1, a_srcImg->GetSize().width, a_srcImg->GetSize().height);
-		}
+	protected:
+
+		MemAccessor_2D<T_AccElm> m_memAccessor;
+		Hcpl::ObjRef< Hcv::Image< T_ImgElm >> m_srcImg;
+
+		//static void PrepareF32ColorValAccessor(F32ImageRef a_srcImg, MemAccessor_2D<F32ColorVal> * a_pAccessor)
+		//{
+		//	Hcpl_ASSERT(3 == a_srcImg->GetNofChannels());
+
+		//	a_pAccessor->Init((F32ColorVal *)a_srcImg->GetPixAt(0, 0),
+		//		1, a_srcImg->GetSize().width, a_srcImg->GetSize().height);
+		//}
+
+		//static void PrepareFloatAccessor(F32ImageRef a_srcImg, MemAccessor_2D<float> * a_pAccessor)
+		//{
+		//	Hcpl_ASSERT(1 == a_srcImg->GetNofChannels());
+
+		//	a_pAccessor->Init((float *)a_srcImg->GetPixAt(0, 0),
+		//		1, a_srcImg->GetSize().width, a_srcImg->GetSize().height);
+		//}
 
 	};
+
+	typedef Hcv::MemAccessor_2D< Hcpl::Float, F32ColorVal, 3 > F32Image3C_F32ColorVal_MemAccessor_2D;
+	typedef Hcv::MemAccessor_2D< Hcpl::Float, float, 1 > F32Image_Float1C_MemAccessor_2D;
+
+	typedef Hcv::MemAccessor_2D< Hcpl::Int32, int, 1 > S32Image1C_Int_MemAccessor_2D;
+
+	
 }
