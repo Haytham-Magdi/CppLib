9067cac7d1e83bf5c808c512f85058d2cca32dba
haytham magdi <haytham.magdi@hotmail.com>
2016-08-20 14:04:57 +0300

    developing ImageUtil functions

--- Hcv/ImageUtil.h
+++ Hcv/ImageUtil.h
@@ -44,6 +44,8 @@ namespace Hcv
 			}
 		}
 
+		//-------------------------------------------------------------------
+
 		template<class T>
 		void FillImage(MemAccessor_2D_REF(T) a_memAcc, T & a_val)
 		{
@@ -70,6 +72,110 @@ namespace Hcv
 				Type_Basic::CopyByPtr<T>(ptr, &a_val);
 			}
 		}
+
+		//-------------------------------------------------------------------
+
+		template<class T>
+		void CopyImage(MemAccessor_2D_REF(T) a_destAcc, MemAccessor_2D_REF(T) a_srcAcc)
+		{
+			MemAccessor_1D_REF(T) acc_Src_Y = a_srcAcc->GenAccessor_1D_Unlocked_Y();
+			MemAccessor_1D_REF(T) acc_Src_X = a_srcAcc->GenAccessor_1D_Unlocked_X();
+
+			MemAccessor_1D_REF(T) acc_Dest_Y = a_destAcc->GenAccessor_1D_Unlocked_Y();
+			MemAccessor_1D_REF(T) acc_Dest_X = a_destAcc->GenAccessor_1D_Unlocked_X();
+
+			Hcpl_ASSERT(acc_Src_Y->GetOffsetCalc()->GetMaxNofSteps() ==
+				acc_Dest_Y->GetOffsetCalc()->GetMaxNofSteps());
+
+			PtrIterator<T> ptrItr_Src_Y = acc_Src_Y->GenPtrIterator(0, 0);
+			PtrIterator<T> ptrItr_Dest_Y = acc_Dest_Y->GenPtrIterator(0, 0);
+
+			int i = 0;
+			T * ptr_Src_Y = ptrItr_Src_Y.GetCurrent();
+			T * ptr_Dest_Y = ptrItr_Dest_Y.GetCurrent();
+			for (;
+				!ptrItr_Src_Y.IsDone();
+				ptr_Src_Y = ptrItr_Src_Y.Next(), ptr_Dest_Y = ptrItr_Dest_Y.Next(), i++)
+			{
+				acc_Src_X->SetDataPtr(ptr_Src_Y);
+				acc_Dest_X->SetDataPtr(ptr_Dest_Y);
+
+				CopyLine<T>(acc_Dest_X, acc_Src_X);
+			}
+		}
+
+		template<class T>
+		void CopyLine(MemAccessor_1D_REF(T) a_destAcc, MemAccessor_1D_REF(T) a_srcAcc)
+		{
+			Hcpl_ASSERT(a_srcAcc->GetOffsetCalc()->GetMaxNofSteps() ==
+				a_destAcc->GetOffsetCalc()->GetMaxNofSteps());
+
+			PtrIterator<T> ptrItr_Src = a_srcAcc->GenPtrIterator(0, 0);
+			PtrIterator<T> ptrItr_Dest = a_destAcc->GenPtrIterator(0, 0);
+
+			T * ptr_Src = ptrItr_Src.GetCurrent();
+			T * ptr_Dest = ptrItr_Dest.GetCurrent();
+			for (;
+				!ptrItr_Src.IsDone();
+				ptr_Src = ptrItr_Src.Next(), ptr_Dest = ptrItr_Dest.Next())
+			{
+				Type_Basic::CopyByPtr<T>(ptr_Dest, ptr_Src);
+			}
+		}
+
+		//-------------------------------------------------------------------
+
+		template<class T>
+		void CalcMagImage(MemAccessor_2D_REF(T) a_outAcc, MemAccessor_2D_REF(T) a_inpAcc)
+		{
+			MemAccessor_1D_REF(T) acc_Inp_Y = a_inpAcc->GenAccessor_1D_Unlocked_Y();
+			MemAccessor_1D_REF(T) acc_Inp_X = a_inpAcc->GenAccessor_1D_Unlocked_X();
+
+			MemAccessor_1D_REF(T) acc_Out_Y = a_outAcc->GenAccessor_1D_Unlocked_Y();
+			MemAccessor_1D_REF(T) acc_Out_X = a_outAcc->GenAccessor_1D_Unlocked_X();
+
+			Hcpl_ASSERT(acc_Inp_Y->GetOffsetCalc()->GetMaxNofSteps() ==
+				acc_Out_Y->GetOffsetCalc()->GetMaxNofSteps());
+
+			PtrIterator<T> ptrItr_Inp_Y = acc_Inp_Y->GenPtrIterator(0, 0);
+			PtrIterator<T> ptrItr_Out_Y = acc_Out_Y->GenPtrIterator(0, 0);
+
+			int i = 0;
+			T * ptr_Inp_Y = ptrItr_Inp_Y.GetCurrent();
+			T * ptr_Out_Y = ptrItr_Out_Y.GetCurrent();
+			for (;
+				!ptrItr_Inp_Y.IsDone();
+				ptr_Inp_Y = ptrItr_Inp_Y.Next(), ptr_Out_Y = ptrItr_Out_Y.Next(), i++)
+			{
+				acc_Inp_X->SetDataPtr(ptr_Inp_Y);
+				acc_Out_X->SetDataPtr(ptr_Out_Y);
+
+				CalcMagLine<T>(acc_Out_X, acc_Inp_X);
+			}
+		}
+
+		template<class T>
+		void CalcMagLine(MemAccessor_1D_REF(T) a_outAcc, MemAccessor_1D_REF(T) a_inpAcc)
+		{
+			Hcpl_ASSERT(a_inpAcc->GetOffsetCalc()->GetMaxNofSteps() ==
+				a_outAcc->GetOffsetCalc()->GetMaxNofSteps());
+
+			PtrIterator<T> ptrItr_Inp = a_inpAcc->GenPtrIterator(0, 0);
+			PtrIterator<T> ptrItr_Out = a_outAcc->GenPtrIterator(0, 0);
+
+			T * ptr_Inp = ptrItr_Inp.GetCurrent();
+			T * ptr_Out = ptrItr_Out.GetCurrent();
+			for (;
+				!ptrItr_Inp.IsDone();
+				ptr_Inp = ptrItr_Inp.Next(), ptr_Out = ptrItr_Out.Next())
+			{
+				Type_Basic::CopyByPtr<T>(ptr_Out, ptr_Inp);
+			}
+		}
+
+		//-------------------------------------------------------------------
+
+
 
 	};
 }
