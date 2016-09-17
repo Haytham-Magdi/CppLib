0a4eec7355c5ed649587391066003a75b1814efd
haytham magdi <haytham.magdi@hotmail.com>
2016-09-17 12:53:30 +0300

    developing VectorVal stuff

---
+++ Hcv/Element_Operations_VectorVal.cpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <Lib\Cpp\Common\commonLib.h>
+//#include <Lib\Hcv\CvIncludes.h>
+#include <Lib\Hcv\Types.h>
+#include <Lib\Hcv\error.h>
+//#include <Lib\Hcv\funcs1.h>
+//#include <vector>
+//#include <Lib\Hcv\Image.h>
+
+#include <Lib\Hcv\Element_Operations.h>
+#include <Lib\Hcv\Element_Operations_VectorVal.h>
+
+
+namespace Hcv
+{
+	using namespace Hcpl;
+
+	namespace Element_Operations
+	{
+
+		void Copy_ByPtr(VectorVal * a_pDest, VectorVal * a_pSrc)
+		{
+			memcpy(a_pDest, a_pSrc, sizeof(VectorVal));
+		}
+
+		float CalcMagSqr_ByPtr(VectorVal * a_pArg)
+		{
+			return a_pArg->CalcMagSqr();
+		}
+
+		void Add_ByPtr(VectorVal * a_pInp1, VectorVal * a_pInp2, VectorVal * a_pOut)
+		{
+			a_pOut->val0 = a_pInp1->val0 + a_pInp2->val0;
+			a_pOut->val1 = a_pInp1->val1 + a_pInp2->val1;
+			a_pOut->val2 = a_pInp1->val2 + a_pInp2->val2;
+		}
+
+		void Subtract_ByPtr(VectorVal * a_pInp1, VectorVal * a_pInp2, VectorVal * a_pOut)
+		{
+			a_pOut->val0 = a_pInp1->val0 - a_pInp2->val0;
+			a_pOut->val1 = a_pInp1->val1 - a_pInp2->val1;
+			a_pOut->val2 = a_pInp1->val2 - a_pInp2->val2;
+		}
+
+		void DivideByNum_ByPtr(VectorVal * a_pInp, float a_num, VectorVal * a_pOut)
+		{
+			a_pOut->val0 = a_pInp->val0 / a_num;
+			a_pOut->val1 = a_pInp->val1 / a_num;
+			a_pOut->val2 = a_pInp->val2 / a_num;
+		}
+
+		void MultiplyByNum_ByPtr(VectorVal * a_pInp, float a_num, VectorVal * a_pOut)
+		{
+			a_pOut->val0 = a_pInp->val0 * a_num;
+			a_pOut->val1 = a_pInp->val1 * a_num;
+			a_pOut->val2 = a_pInp->val2 * a_num;
+		}
+
+		void SetToZero_ByPtr(VectorVal * a_pArg)
+		{
+			a_pArg->val0 = 0;
+			a_pArg->val1 = 0;
+			a_pArg->val2 = 0;
+		}
+
+	};
+}
---
+++ Hcv/Element_Operations_VectorVal.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Lib\Cpp\Common\commonLib.h>
+//#include <Lib\Hcv\CvIncludes.h>
+#include <Lib\Hcv\Types.h>
+#include <Lib\Hcv\error.h>
+#include <Lib\Hcv\funcs1.h>
+//#include <vector>
+//#include <Lib\Hcv\Image.h>
+
+#include <Lib\Hcv\Element_Operations.h>
+
+
+namespace Hcv
+{
+	using namespace Hcpl;
+
+	namespace Element_Operations
+	{
+
+		template<> void Copy_ByPtr<VectorVal>(VectorVal * a_pDest, VectorVal * a_pSrc);
+
+		template<> float CalcMagSqr_ByPtr(VectorVal * a_pArg);
+
+		template<> void Add_ByPtr(VectorVal * a_pInp1, VectorVal * a_pInp2, VectorVal * a_pOut);
+
+		template<> void Subtract_ByPtr(VectorVal * a_pInp1, VectorVal * a_pInp2, VectorVal * a_pOut);
+
+		template<> void DivideByNum_ByPtr(VectorVal * a_pInp, float a_num, VectorVal * a_pOut);
+		
+		template<> void MultiplyByNum_ByPtr(VectorVal * a_pInp, float a_num, VectorVal * a_pOut);
+
+		template<> void SetToZero_ByPtr(VectorVal * a_pArg);
+
+	};
+}
