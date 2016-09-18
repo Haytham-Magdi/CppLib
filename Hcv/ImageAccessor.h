5e9aa563000d36c350b7e5f9a74de13b3cc77b16
haytham magdi <haytham.magdi@hotmail.com>
2016-09-18 06:49:04 +0300

    cmt

--- Hcv/ImageAccessor.h
+++ Hcv/ImageAccessor.h
@@ -219,29 +219,42 @@ namespace Hcv
 
 	//template<class T_ImgElm, class T_AccElm, int const V_NofChannels>
 	template<class T, int const V_NofChannels>
-	class VectorValImageAcc : public ImageAccessor<T, VectorVal<T, N>, N>
+	class VectorValImageAcc : public ImageAccessor<T, VectorVal<T, V_NofChannels>, V_NofChannels>
 	{
+	public:
+		VectorValImageAcc(IMAGE_REF(T) a_srcImg) : base(a_srcImg)
+		{
+			//m_isLocked = false;
+			//Init(a_srcImg);
+		}
+
+		VectorValImageAcc(OffsetCalc_2D_Ref a_offsetCalc) : base(a_offsetCalc)
+		{
+			//m_isLocked = false;
+			//Init(a_offsetCalc);
+		}
 	};
 
 	//template<class T_ImgElm, class T_AccElm, int const V_NofChannels>
 	template<class T, int const V_NofChannels>
 	class VectorValImageAcc_Ref : public ObjRef< VectorValImageAcc<T, V_NofChannels> >
 	{
+	public:
 	};
 
-//	typedef VectorValImageAcc< Float, 4 > F32VectorValImageAcc_4C;
-//	typedef ObjRef< F32VectorValImageAcc_4C > F32VectorValImageAcc_4C_Ref;
-//
-//	typedef VectorValImageAcc< Float, 3 > F32VectorValImageAcc_3C;
-//	typedef ObjRef< F32VectorValImageAcc_3C > F32VectorValImageAcc_3C_Ref;
-//
-//	typedef VectorValImageAcc< Float, 1 > F32VectorValImageAcc_1C;
-//	typedef ObjRef< F32VectorValImageAcc_1C > F32VectorValImageAcc_1C_Ref;
-//
-//	typedef VectorValImageAcc< Int32, 1 > S32VectorValImageAcc_1C;
-//	typedef ObjRef< S32VectorValImageAcc_1C > S32VectorValImageAcc_1C_Ref;
-//
-//#define VectorValImageAcc(T, N) ImageAccessor_REF(T, VectorVal<T, N>, N) ;  S32ImageAccessor1C;
+	typedef VectorValImageAcc< Float, 4 > F32VectorValImageAcc_4C;
+	typedef ObjRef< F32VectorValImageAcc_4C > F32VectorValImageAcc_4C_Ref;
+
+	typedef VectorValImageAcc< Float, 3 > F32VectorValImageAcc_3C;
+	typedef ObjRef< F32VectorValImageAcc_3C > F32VectorValImageAcc_3C_Ref;
+
+	typedef VectorValImageAcc< Float, 1 > F32VectorValImageAcc_1C;
+	typedef ObjRef< F32VectorValImageAcc_1C > F32VectorValImageAcc_1C_Ref;
+
+	typedef VectorValImageAcc< Int32, 1 > S32VectorValImageAcc_1C;
+	typedef ObjRef< S32VectorValImageAcc_1C > S32VectorValImageAcc_1C_Ref;
+
+	//#define VectorValImageAcc(T, N) ImageAccessor_REF(T, VectorVal<T, N>, N) ;  S32ImageAccessor1C;
 
 
 	//#define VectorValImageAcc_REF(T, N) ImageAccessor_REF(T, VectorVal<T, N>, N) ;  S32ImageAccessor1C;
