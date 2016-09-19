213b558c0460f22a97f840f275f243de2669d49b
haytham magdi <haytham.magdi@hotmail.com>
2016-09-19 08:25:22 +0300

    cmt

--- Hcv/Element_Operations_VectorVal.h
+++ Hcv/Element_Operations_VectorVal.h
@@ -21,8 +21,14 @@ namespace Hcv
 
 		//template<> void Copy_ByPtr<VectorVal>(VectorVal * a_pDest, VectorVal * a_pSrc);
 
-		template<class T, const int N>
-		float CalcMagSqr_ByPtr(VectorVal<T, N> * a_pArg)
+		//template<>
+		//float CalcMagSqr_ByPtr(VectorVal<T, N> * a_pArg)
+
+		template<class T2, const int N>
+		//template<>
+		template
+		//template<class T2, const int N>
+		float CalcMagSqr_ByPtr<VectorVal<T2, N>>(VectorVal<T2, N> * a_pArg)
 		{
 			float sum = 0;
 			for (int i = 0; i < a_pArg->GetNofDims(); i++)
@@ -33,58 +39,56 @@ namespace Hcv
 			return sum;
 		}
 
-		
-		template<class T, const int N>
-		void Add_ByPtr(VectorVal<T, N> * a_pInp1, VectorVal<T, N> * a_pInp2, VectorVal<T, N> * a_pOut)
-		{
-			for (int i = 0; i < a_pArg->GetNofDims(); i++)
-			{
-				a_pOut->Vals[i] = a_pInp1->Vals[i] + a_pInp2->Vals[i];
-			}
-		}
+
+		//template<class T, const int N>
+		//void Add_ByPtr(VectorVal<T, N> * a_pInp1, VectorVal<T, N> * a_pInp2, VectorVal<T, N> * a_pOut)
+		//{
+		//	for (int i = 0; i < a_pArg->GetNofDims(); i++)
+		//	{
+		//		a_pOut->Vals[i] = a_pInp1->Vals[i] + a_pInp2->Vals[i];
+		//	}
+		//}
 
 
-		template<class T, const int N>
-		void Subtract_ByPtr(VectorVal<T, N> * a_pInp1, VectorVal<T, N> * a_pInp2, VectorVal<T, N> * a_pOut)
-		{
-			for (int i = 0; i < a_pArg->GetNofDims(); i++)
-			{
-				a_pOut->Vals[i] = a_pInp1->Vals[i] - a_pInp2->Vals[i];
-			}
-		}
+		//template<class T, const int N>
+		//void Subtract_ByPtr(VectorVal<T, N> * a_pInp1, VectorVal<T, N> * a_pInp2, VectorVal<T, N> * a_pOut)
+		//{
+		//	for (int i = 0; i < a_pArg->GetNofDims(); i++)
+		//	{
+		//		a_pOut->Vals[i] = a_pInp1->Vals[i] - a_pInp2->Vals[i];
+		//	}
+		//}
 
 
-		
-		template<class T, const int N>
-		void DivideByNum_ByPtr(VectorVal<T, N> * a_pInp, float a_num, VectorVal<T, N> * a_pOut)
-		{
-			for (int i = 0; i < a_pArg->GetNofDims(); i++)
-			{
-				a_pOut->Vals[i] = a_pInp1->Vals[i] + a_pInp2->Vals[i];
-			}
-		}
-
-		
-		
-		template<class T, const int N>
-		void MultiplyByNum_ByPtr(VectorVal<T, N> * a_pInp, float a_num, VectorVal<T, N> * a_pOut)
-		{
-			for (int i = 0; i < a_pArg->GetNofDims(); i++)
-			{
-				a_pOut->Vals[i] = a_pInp1->Vals[i] + a_pInp2->Vals[i];
-			}
-		}
+		//template<class T, const int N>
+		//void DivideByNum_ByPtr(VectorVal<T, N> * a_pInp, float a_num, VectorVal<T, N> * a_pOut)
+		//{
+		//	for (int i = 0; i < a_pArg->GetNofDims(); i++)
+		//	{
+		//		a_pOut->Vals[i] = a_pInp->Vals[i] / a_num;
+		//	}
+		//}
 
 
-		
-		template<class T, const int N>
-		void SetToZero_ByPtr(VectorVal<T, N> * a_pArg)
-		{
-			for (int i = 0; i < a_pArg->GetNofDims(); i++)
-			{
-				a_pOut->Vals[i] = a_pInp1->Vals[i] + a_pInp2->Vals[i];
-			}
-		}
+		//template<class T, const int N>
+		//void MultiplyByNum_ByPtr(VectorVal<T, N> * a_pInp, float a_num, VectorVal<T, N> * a_pOut)
+		//{
+		//	for (int i = 0; i < a_pArg->GetNofDims(); i++)
+		//	{
+		//		a_pOut->Vals[i] = a_pInp->Vals[i] * a_num;
+		//	}
+		//}
+
+
+
+		//template<class T, const int N>
+		//void SetToZero_ByPtr(VectorVal<T, N> * a_pArg)
+		//{
+		//	for (int i = 0; i < a_pArg->GetNofDims(); i++)
+		//	{
+		//		a_pArg->Vals[i] = 0;
+		//	}
+		//}
 
 
 	};
