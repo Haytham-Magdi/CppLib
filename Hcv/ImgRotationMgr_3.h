7cea455e38fbdfd7a5f7ac15aaf827fc6f6c8551
haytham magdi <haytham.magdi@hotmail.com>
2016-09-30 11:21:22 +0200

    developing ImgRotationMgr

---
+++ Hcv/ImgRotationMgr_3.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <Lib\Cpp\Common\commonLib.h>
+#include <Lib\Cpp\Math\mathLib.h>
+#include <Lib\Hcv\CvIncludes.h>
+#include <Lib\Hcv\Types.h>
+#include <Lib\Hcv\error.h>
+#include <vector>
+#include <Lib\Hcv\Channel.h>
+#include <Lib\Hcv\Image.h>
+
+//#include <Lib\Hcv\ImageLineItrProvider.h>
+#include <Lib\Hcv\ImageItrMgr.h>
+
+#define M_PI 3.14159265358979323846
+
+
+namespace Hcv
+{
+
+	class ImgRotationMgr  : FRM_Object
+	{
+	public:
+
+		ImgRotationMgr( F32ImageRef a_srcImg, float a_angDig );
+
+		F32ImageRef GetSrcImg()
+		{			
+			return m_srcImg;
+		}
+
+		float GetAngleByDigree()
+		{
+			return m_angDig;
+		}
+
+		float GetAngleByRad()
+		{
+			return m_angRad;
+		}
+
+		CvSize GetResImgSiz()
+		{
+			return m_resSiz;
+		}
+
+		S32ImageRef Get_ResToSrcMapImage()
+		{
+			return m_resToSrcMapImg;
+		}
+
+		//S32ImageRef Get_ResToSrcMap_X_Scaled_Image()
+		//{
+		//	return m_resToSrcMapImg_X_Scaled;
+		//}
+
+		//S32ImageRef Get_ResToSrcMap_Y_Scaled_Image()
+		//{
+		//	return m_resToSrcMapImg_Y_Scaled;
+		//}
+
+		F32ImageRef GetResImg()
+		{
+			return m_resImg;
+		}
+
+		S32ImageRef Get_SrcToResMapImage()
+		{
+			return m_srcToResMapImg;
+		}
+
+		ImageItrMgrRef GetImageItrMgr()
+		{
+			return m_imageItrMgr;
+		}
+
+	protected:
+
+		void Prepare();
+		
+		//void PrepareResImg();
+
+		int AddRound( int a_num );
+
+		int AddRoundByMin( int a_num );
+		
+		//void PrepareProvArr();
+		void PrepareImageItrMgr();
+		
+
+
+	protected:
+
+		F32ImageRef m_srcImg;
+		float m_angDig;
+		float m_angRad;
+
+		int m_nScale;
+		int m_nRound;
+		int m_nRoundByMin;
+		int m_nCos;
+		int m_nSin;
+
+		CvSize m_resSiz;
+		S32ImageRef m_resToSrcMapImg;
+		//S32ImageRef m_resToSrcMapImg_X_Scaled;
+		//S32ImageRef m_resToSrcMapImg_Y_Scaled;
+		F32ImageRef m_resImg;
+		S32ImageRef m_srcToResMapImg;
+
+		FixedVector< LineLimit > m_lineLimit_H_Arr;
+		FixedVector< LineLimit > m_lineLimit_V_Arr;
+
+		//FixedVector< ImageLineItrProvider > m_lineItrProv_Arr;
+
+		ImageItrMgrRef m_imageItrMgr;
+
+		
+	};
+
+
+	typedef Hcpl::ObjRef< ImgRotationMgr > ImgRotationMgrRef;
+
+}
